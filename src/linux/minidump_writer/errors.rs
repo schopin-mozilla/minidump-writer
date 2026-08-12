0648d292b387ac925e4c4bec37535fa27440bbdb
Simon Chopin <schopin@mozilla.com>
2026-08-12 12:49:57 +0200

    linux: optionally get module data from the debugger rendez-vous
    Wire up a config toggle to switch to the rendezvous data source.
    
    If the primary source fails, log the error and fall back to the
    secondary source.

--- src/linux/minidump_writer/errors.rs
+++ src/linux/minidump_writer/errors.rs
@@ -12,7 +12,7 @@ use {
             thread_list_stream::SectionThreadListError,
             thread_names_stream::SectionThreadNamesError,
         },
-        module_list::ModuleResolveError,
+        module_list::{DebuggerRendezvousError, ModuleResolveError},
         module_reader::ModuleReaderError,
         process_inspection,
         serializers::*,
@@ -185,6 +185,14 @@ pub enum InitError {
     SuspendNoThreadsLeft(usize),
     #[error("Crash thread does not reference principal mapping")]
     PrincipalMappingNotReferenced,
+    #[error("no program header table address in auxiliary vector")]
+    MissingProgramHeaderTableAddress,
+    #[error("no program header count in auxiliary vector")]
+    MissingProgramHeaderCount,
+    #[error("failed to obtain the module list from the debugger rendez-vous")]
+    ModuleListFromDebuggerRendezvousFailed(#[source] DebuggerRendezvousError),
+    #[error("Errors occurred while walking the debugger rendez-vous")]
+    DebuggerRendezvousErrors(#[source] ErrorList<DebuggerRendezvousError>),
 }
 
 #[derive(Debug, thiserror::Error, serde::Serialize)]
